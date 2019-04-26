385fd2096c0b2fa433b1e5d307b310490a0c1dc8
Augusto <dev.augusto@gmail.com>
2019-04-26 00:02:22 -0300

    Initial commit

---
+++ app/src/main/java/aucamana/com/br/yourpocketgym/LoginActivity.kt
@@ -0,0 +1,12 @@
+package aucamana.com.br.yourpocketgym
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
